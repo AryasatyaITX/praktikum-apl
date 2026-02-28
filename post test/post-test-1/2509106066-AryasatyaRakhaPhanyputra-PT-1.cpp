6d07fc926dd173ddfaff4c7104d4d7e35c835a06
AryasatyaITX <aoshic53@gmail.com>
2026-02-28 14:06:30 +0800

    Bismillah Finish Post Test 1

---
+++ post test/post-test-1/2509106066-AryasatyaRakhaPhanyputra-PT-1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string username;
+    string password;
+    int kesempatan;
+    int pilih;
+    kesempatan = 0;
+
+    while(kesempatan < 3) {
+        cout << "Masukkan username : ";
+        cin >> username;
+        cout << "Masukkan password : ";
+        cin >> password;
+        if(username == "Arya" && password == "066") {
+            cout << "=====================================\n";
+            cout << "|           Login Berhasil!         |\n";
+            cout << "|       Selamat datang " << username << "!        |\n";
+            cout << "=====================================\n" << endl;
+            break;
+        } else {
+            kesempatan++;
+            cout << "Login Gagal! username atau password anda salah, silahkan coba lagi!. Percobaan ke " << kesempatan << " dari 3\n" << endl;
+        }
+        if (kesempatan == 3){
+            cout << "=====================================\n";
+            cout << "| Kesempatan login anda sudah habis.|\n";
+            cout << "=====================================\n";
+            cout << "Program dihentikan.\n"<< endl;
+            return 0;
+        }
+    }
+
+    do {
+        cout << "====================================\n";
+        cout << "|         MENU KONVERSI WAKTU      |\n";
+        cout << "====================================\n";
+        cout << "|1. Jam -> Menit & Detik           |\n";
+        cout << "|2. Menit -> Jam & Detik           |\n";
+        cout << "|3. Detik -> Jam & Menit           |\n";
+        cout << "|4. Keluar                         |\n";
+        cout << "====================================\n";
+        cout << "Pilih menu konversi waktu yang diinginkan : "<< endl;
+        cin >> pilih;
+
+        if(pilih == 1){
+            float jam;
+            cout << "Masukkan Jam : ";
+            cin >> jam;
+            cout << "Hasil Konversi:\n";
+            cout << jam << " Jam = ";
+            cout << jam * 60 << " Menit";
+            cout << " dan ";
+            cout << jam * 3600 << " Detik\n";
+            cout << "Konversi selesai kembali ke menu\n"<< endl;            
+        }
+        else if(pilih == 2){
+            int menit;
+            int sisaMenit;
+            cout << "Masukkan Menit : ";
+            cin >> menit;
+            cout << "Hasil Konversi:\n";
+            cout << menit / 60 << " Jam";
+            cout << " ";
+            sisaMenit = menit % 60;
+            cout << sisaMenit << " Menit";
+            cout << " dan ";
+            cout << menit * 60 << " Detik\n";
+            cout << "Konversi selesai kembali ke menu\n"<< endl;
+        }
+        else if(pilih == 3){
+            int detik;
+            int sisaDetik;
+            int SisaMenit;
+            cout << "Masukkan Detik : ";
+            cin >> detik;
+            cout << "Hasil Konversi:\n";
+            cout << detik << " Detik = ";
+            cout << detik / 3600 << " Jam ";
+            SisaMenit = detik % 3600;
+            cout << SisaMenit / 60 << " Menit , ";
+            cout << detik / 60 << " Menit dan \n";
+            sisaDetik = detik % 60;
+            cout << sisaDetik << " Detik\n";
+            cout << "Konversi selesai kembali ke menu\n"<< endl;
+        }
+        else if(pilih == 4){
+            cout << "================================\n";
+            cout << "|         TERIMA KASIH!        |\n";
+            cout << "|program selesai, sampai jumpa!|\n";
+            cout << "================================\n";
+        }
+        else{
+            cout << "Pilihan menu tidak tersedia!\n"<< endl;
+        }
+    } while(pilih != 4);
+    return 0;
+}
