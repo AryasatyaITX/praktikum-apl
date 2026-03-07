2dc31adce3340a8e21761b604b720cd150501d63
AryasatyaITX <aoshic53@gmail.com>
2026-03-07 21:04:26 +0800

    perbaikan bug crud

--- post test/post-test-2/2509106066-AryasatyaRakhaPhanyputra-PT-2.cpp
+++ post test/post-test-2/2509106066-AryasatyaRakhaPhanyputra-PT-2.cpp
@@ -25,7 +25,7 @@ struct Lightnovel{
 Lightnovel novel[MAX_LIGHTNOVEL] = {
     {1,"Sword Art Online","Reki Kawahara",{"Fantasy",27,"Ongoing"}},
     {2,"Overlord","Kugane Maruyama",{"Isekai",17,"Ongoing"}},
-    {3,"Classroom of the Elite","Syougo Kinugasa",{"School",14,"Ongoing"}}
+    {3,"Classroom of the Elite","Syougo Kinugasa",{"School",31,"Ongoing"}}
 };
 
 int jumlahNovel = 3;
